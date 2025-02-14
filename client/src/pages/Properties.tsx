cf6b90b1c503769bd4f7ae017bdb4dd6149725c9
kunal khairnar <kunalkhairnar@kunals-MacBook-Air.local>
2025-02-14 17:51:11 +0530

    Added properties on client side

--- client/src/pages/Properties.tsx
+++ client/src/pages/Properties.tsx
@@ -2,11 +2,55 @@ import { useQuery } from "@tanstack/react-query";
 import PropertyCard from "@/components/PropertyCard";
 import { Card, CardContent } from "@/components/ui/card";
 import type { Property } from "@shared/schema";
+import { useEffect } from "react";
 
 export default function Properties() {
-  const { data: properties, isLoading } = useQuery<Property[]>({
-    queryKey: ["/api/properties"]
-  });
+  // const { data: properties, isLoading } = useQuery<Property[]>({
+  //   queryKey: ["/api/properties"]
+  // });
+  const properties=[
+    {
+      id:123,
+      title: "Premium NA Plot in Nashik",
+      description: "Well-developed NA plot with excellent connectivity",
+      price: "₹50 Lakhs",
+      area: "2000 sq.ft",
+      location: "Nashik",
+      type: "NA Plot",
+      imageUrl: "https://is1-3.housingcdn.com/01c16c28/08cbb671362e9ed978ef1f82f5194a6b/v0/fs/residential_plot-for-sale-nashik_road-Nashik-plot_view.jpg",
+      features: ["Corner Plot", "Ready for Construction", "All Utilities"]
+    },
+    {
+      id:1234,
+      title: "Agricultural Land in Dindori",
+      description: "Fertile agricultural land with water source",
+      price: "₹30 Lakhs",
+      area: "1 Acre",
+      location: "Ozar",
+      type: "Agricultural",
+      imageUrl: "https://5.imimg.com/data5/ANDROID/Default/2021/4/BD/XV/AD/49810555/product-jpeg-500x500.jpg",
+      features: ["Fertile Soil", "Water Source", "Road Access"]
+    },
+    {
+      id:1235,
+      title: "Agricultural Land in Ozar",
+      description: "Fertile agricultural land",
+      price: "₹80 Lakhs",
+      area: "2 Acre",
+      location: "Ozar",
+      type: "Agricultural",
+      imageUrl: "https://5.imimg.com/data5/EI/MZ/KK/SELLER-1419216/agriculture-land-sale-500x500.jpg",
+      features: ["Fertile Soil", "Water Source", "Road Access"]
+    }
+  ]
+var isLoading=false;
+  useEffect(()=>{
+    isLoading=true;
+    setTimeout(()=>{
+      isLoading=false;
+    },1000)
+   
+  },[])
 
   if (isLoading) {
     return (
